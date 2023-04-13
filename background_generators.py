550c18e8533e192e98f0d260e2e29573cbc2cdb8
Lucas <lucas.ewing2000@gmail.com>
2023-04-12 17:15:22 -0700

    Added actual backgrounds too and split up code

---
+++ background_generators.py
@@ -0,0 +1,75 @@
+import cv2
+import os
+import random
+import numpy as np
+
+def get_random_background_image(background_images_dir, used_backgrounds):
+    """
+    Selects a random background image from the given background_images_dir, while ensuring that it has not been used before.
+    
+    :param background_images_dir: Directory containing background images.
+    :type background_images_dir: str
+    :param used_backgrounds: Set of filenames of the used background images.
+    :type used_backgrounds: set
+    :return: A random background image from the directory.
+    :rtype: numpy.ndarray
+    :raises ValueError: If there are no available background images in the directory.
+    """
+    available_images = list(set(os.listdir(background_images_dir)) - set(used_backgrounds))
+    if not available_images:
+        raise ValueError("No available background images.")
+    
+    chosen_image = random.choice(available_images)
+    used_backgrounds.add(chosen_image)
+    
+    return cv2.imread(os.path.join(background_images_dir, chosen_image), cv2.IMREAD_COLOR)
+
+def generate_random_background(shape):
+    """
+    Generates a random background image by choosing between solid color, striped, and blended color backgrounds.
+    
+    :param shape: A tuple of the form (height, width, channels) representing the desired shape of the output background image.
+    :type shape: tuple
+    :return: A randomly generated background image with the specified shape.
+    :rtype: numpy.ndarray
+    """
+    choice = np.random.choice(["solid", "striped", "blended"])
+
+    if choice == "solid":
+        # Create a random solid color background
+        bg_color = np.random.randint(0, 256, size=3, dtype=np.uint8)
+        bg = np.full((shape[0], shape[1], 3), bg_color, dtype=np.uint8)
+
+    elif choice == "striped":
+        # Create a striped background with random colors and directions
+        num_slices = np.random.randint(2, 6)  # Choose the number of color slices (2 to 5)
+        slice_width = shape[1] // num_slices
+        slices = []
+
+        for i in range(num_slices):
+            color = np.random.randint(0, 256, size=3, dtype=np.uint8)
+            single_slice = np.full((shape[0], slice_width, 3), color, dtype=np.uint8)
+            slices.append(single_slice)
+
+        # Concatenate the color slices to form the striped background
+        bg = np.hstack(slices)
+
+        # If the width of the concatenated slices is smaller than the original image width,
+        # extend the last slice to match the width
+        if bg.shape[1] < shape[1]:
+            last_slice = np.full((shape[0], shape[1] - bg.shape[1], 3), slices[-1][0, 0], dtype=np.uint8)
+            bg = np.hstack((bg, last_slice))
+
+        # Rotate the stripes randomly between 0, 45, 90, and 135 degrees
+        rotation_angle = np.random.choice([0, 45, 90, 135])
+        rotation_matrix = cv2.getRotationMatrix2D((shape[1] // 2, shape[0] // 2), rotation_angle, 1)
+        bg = cv2.warpAffine(bg, rotation_matrix, (shape[1], shape[0]))
+
+    elif choice == "blended":
+        # Create a random color background
+        bg_color = np.random.randint(0, 256, size=(shape[0], shape[1], 3), dtype=np.uint8)
+
+        # Apply a Gaussian blur to the background to create a blend of colors
+        bg = cv2.GaussianBlur(bg_color, (25, 25), 10)
+
+    return bg
---
+++ image_processing.py
@@ -0,0 +1,105 @@
+import cv2
+import numpy as np
+import os
+from background_generators import generate_random_background, get_random_background_image
+
+def create_binary_mask(image, mask_output_dir, index):
+    """
+    Creates a binary mask from the given image and saves it to the specified directory.
+
+    :param image: input image in BGR format with an alpha channel
+    :param mask_output_dir: output directory for the binary mask
+    :param index: index of the image used for naming the output file
+    """
+    # Extract the alpha channel from the image
+    alpha_channel = image[:, :, 3]
+
+    # Create a binary mask using the alpha channel
+    binary_mask = cv2.threshold(alpha_channel, 0, 255, cv2.THRESH_BINARY)[1]
+
+    # Save the binary mask
+    output_path = os.path.join(mask_output_dir, f"{index}_mask.png")
+    cv2.imwrite(output_path, binary_mask)
+
+def add_selected_background(image, bg_output_dir, index, background_images_dir, used_backgrounds):
+    """
+    Adds a randomly selected background image to the input image and saves the result.
+
+    :param image: input image in BGR format with an alpha channel
+    :param bg_output_dir: output directory for the image with the selected background
+    :param index: index of the image used for naming the output file
+    :param background_images_dir: directory containing background images
+    :param used_backgrounds: set of used background image filenames to prevent repetition
+    """
+    # Get a random background image from the background_images_dir
+    background_image = get_random_background_image(background_images_dir, used_backgrounds)
+
+    # Combine the original image with the selected background
+    foreground = image.copy()    
+    background = cv2.resize(background_image, (image.shape[1], image.shape[0]))
+    alpha = foreground[:, :, 3] / 255.0
+    alpha_expanded = np.repeat(alpha[:, :, np.newaxis], 3, axis=2)
+
+    # Blend the foreground and background images using the alpha channel
+    result = cv2.multiply(alpha_expanded, foreground[:, :, :3].astype(float)) + \
+             cv2.multiply(1 - alpha_expanded, background.astype(float))
+    result = result.astype(np.uint8)
+
+    # Save the image with the selected background
+    output_path = os.path.join(bg_output_dir, f"{index}_with-selected-bg.png")
+    cv2.imwrite(output_path, result)
+
+def add_random_background(image, bg_output_dir, index):
+    """
+    Generates a random background, adds it to the input image, and saves the result.
+
+    :param image: input image in BGR format with an alpha channel
+    :param bg_output_dir: output directory for the image with the random background
+    :param index: index of the image used for naming the output file
+    """
+    # Generate a random background
+    random_bg = generate_random_background(image.shape)
+
+    # Combine the original image with the random background
+    foreground = cv2.cvtColor(image, cv2.COLOR_BGRA2RGBA)
+    background = cv2.cvtColor(random_bg, cv2.COLOR_BGR2RGB)
+    alpha = foreground[:, :, 3] / 255.0
+    alpha_expanded = np.repeat(alpha[:, :, np.newaxis], 3, axis=2)
+    
+    # Blend the foreground and background images using the alpha channel
+    result = cv2.multiply(alpha_expanded, foreground[:, :, :3].astype(float)) + \
+             cv2.multiply(1 - alpha_expanded, background.astype(float))
+    result = result.astype(np.uint8)
+
+    # Save the image with the random background
+    output_path = os.path.join(bg_output_dir, f"{index}_with-bg.png")
+    cv2.imwrite(output_path, cv2.cvtColor(result, cv2.COLOR_RGB2BGR))
+    # print(f"Image with random background saved at {output_path}")
+
+def process_images(input_dir, mask_output_dir, bg_output_dir, background_images_dir):
+    """
+    Processes all images in the input directory, creates binary masks, adds random and selected backgrounds,
+    and saves the results in the specified output directories.
+
+    :param input_dir: directory containing input images
+    :param mask_output_dir: output directory for binary masks
+    :param bg_output_dir: output directory for images with added backgrounds
+    :param background_images_dir: directory containing background images
+    """
+    # Create the output directories if they don't exist
+    if not os.path.exists(mask_output_dir):
+        os.makedirs(mask_output_dir)
+    if not os.path.exists(bg_output_dir):
+        os.makedirs(bg_output_dir)
+
+    used_backgrounds = set()
+    # Loop through all images in the input directory
+    for index, filename in enumerate(os.listdir(input_dir), start=1):
+        if index % 10 == 0:
+            print(f"Processed {index} images")
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            input_image_path = os.path.join(input_dir, filename)
+            image = cv2.imread(input_image_path, cv2.IMREAD_UNCHANGED)
+            create_binary_mask(image, mask_output_dir, index)
+            add_random_background(image, bg_output_dir, index)
+            add_selected_background(image, bg_output_dir, index, background_images_dir, used_backgrounds)
