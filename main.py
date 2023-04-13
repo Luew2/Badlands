f4d9d605e63e3fcddfe2c7b7c38013b597eb746a
Lucas <lucas.ewing2000@gmail.com>
2023-04-12 18:33:07 -0700

    update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from image_processing import process_images
 
 if __name__ == "__main__":
-    input_dir = "stickers"
-    mask_output_dir = "modified_images/mask_2"
-    bg_output_dir = "modified_images/with_bg_2"
+    input_dir = "original_images_large"
+    mask_output_dir = "modified_images/mask_large"
+    bg_output_dir = "modified_images/with_bg_large"
     background_images_dir = "background_images"
 
     print("Processing images...")
